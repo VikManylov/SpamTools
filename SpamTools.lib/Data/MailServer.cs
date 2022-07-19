ea336afe974c3aa19e6569ffa87c35722f8e403b
Viktor <winner1.92@mail.ru>
2022-07-19 16:01:38 +0700

    Добавил в SpamTools.lib папкe Data с классами для работы с отправителями и получателями, и папку Servise c классом для шифрования и дешифрования паролей отправителей

---
+++ SpamTools.lib/Data/MailServer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpamTools.lib.Data
+{
+    public class MailServer
+    {
+        public string Address { get; set; }
+        public int Port { get; set; } = 25;
+        public bool UseSSL { get; set; } = true;
+        public MailServer() { }
+
+        public MailServer(string Address, int Port = 25, bool SSL = true)
+        {
+            this.Address = Address;
+            this.Port = Port;
+            this.UseSSL = SSL;
+        }
+    }
+}
---
+++ SpamTools.lib/Data/Senders.cs
@@ -0,0 +1,19 @@
+﻿using SpamTools.lib.Servise;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpamTools.lib.Data
+{
+    public class Senders
+    {
+        public static List<Sender> List { get; } = new List<Sender>
+        {
+            new Sender{Name = "Ivanov", Address = "ivanov@mail.ru", Password = PasswordService.Encode("PassWord")},
+            new Sender{Name = "Petrov", Address = "petrov@mail.ru", Password = PasswordService.Encode("Pass123")},
+            new Sender{Name = "Sidorov", Address = "sidorov@mail.ru", Password = PasswordService.Encode("qwerty123")}
+        };
+    }
+}
